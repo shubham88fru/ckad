8b97091ce7a6ad5f431ae26d2dba83a24954e8f9
IAmAPro <shubham88fru@yahoo.com>
2022-07-21 14:57:48 +0530

    Create training.sh

---
+++ training.sh
@@ -0,0 +1,18 @@
+source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+
+alias k=kubectl
+complete -o default -F __start_kubectl k
+
+# short alias to set/show context/namespace (only works for bash and bash-compatible shells, current context to be set before using kn to set namespace) 
+alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
+alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+
+
+alias kga="kubectl get all"
+export do = "--dry-run=client -o yaml"
+export kfc="kubectl apply -f"
+
+
+#to execute the bash changes
+. .bashrc
